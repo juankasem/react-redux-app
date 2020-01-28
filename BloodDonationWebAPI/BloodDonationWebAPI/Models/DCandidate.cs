b832046eb9ca5f1d50b5a47f5171c4f1073b8255
Juan Kasem <46277727+juankasem@users.noreply.github.com>
2020-01-28 22:25:06 +0300

    Add asp.net core solution files

---
+++ BloodDonationWebAPI/BloodDonationWebAPI/Models/DCandidate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodDonationWebAPI.Models
+{
+    public class DCandidate
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName ="nvarchar(100)")]
+        public string FullName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Email { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Mobile { get; set; }
+
+        public int Age { get; set; }
+
+        [Column(TypeName = "nvarchar(2)")]
+        public string BloodGroup { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Address { get; set; }
+
+    }
+}
